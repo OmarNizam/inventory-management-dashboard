dbe6aea91572ee3e4b4ba11b6dc2e48dd79ba0c5
Omar Nizam Aldeen <eng.omarnizam@gmail.com>
2026-01-28 18:54:09 +0100

    chore: initialize project with Nuxt.js setup
    - Add package.json with scripts and dependencies for Nuxt.js application
    - Include favicon.ico for branding
    - Create robots.txt to manage web crawler access
    - Add tsconfig.json for TypeScript configuration

---
+++ app/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Button from '~/components/ui/button/Button.vue';
+
+</script>
+
+<template>
+  <div>
+    <h1>Welcome to your Nuxt Dashboard!</h1>
+    <p>This is the home page. If you see this, routing is working!</p>
+    <Button>Click Me</Button>
+  </div>
+</template>
