526da877e976699d3ad2162a482e45425131327a
Omar Nizam Aldeen <eng.omarnizam@gmail.com>
2026-01-29 04:02:04 +0100

    feat: update ESLint configuration to include additional rules and refine ignores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,5 +2,15 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt()
     .prepend({
-        ignores: ['.nuxt/**', '.output/**', 'dist/**', 'node_modules/**', '**/*.min.js', 'app/**']
+        ignores: ['.nuxt/**', '.output/**', 'dist/**', 'node_modules/**', '**/*.min.js', 'src/app/**']
     })
+    .extend('eslint:recommended')
+    .extend('plugin:vue/vue3-recommended')
+    .override({
+        files: ['*.vue'],
+        rules: {
+            'vue/multi-word-component-names': 'off'
+        }
+    })
+    .rule('no-console', 'warn')
+    .rule('no-debugger', 'warn')
